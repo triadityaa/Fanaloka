1f1556cfb8eb57147439ccbe6538c43845c127ec
Tri Aditya <triadityaagustiawan.ta@gmail.com>
2025-06-22 21:09:50 +0800

    edit home screen

--- fanaloka/lib/home.dart
+++ fanaloka/lib/home.dart
@@ -73,17 +73,12 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+              'Fanaloka',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
